076db712dcee33cedce8981912bc610dbd3c803e
DmitryLoginov <daimom25@gmail.com>
2024-02-23 13:43:56 +0300

    UI (MainWindow): created basic UI

---
+++ ValidationComparer/ValidationComparer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ValidationComparer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadValidationResult_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Title = "Загрузить файл",
+                Filter = "CSV|*.csv|Excel|*.xlsx"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                MessageBox.Show("loaded");
+            }
+        }
+    }
+}
